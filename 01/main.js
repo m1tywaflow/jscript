2fc16e056b280691dfddeaeeea053d7e590549f4
m1tywaflow <dimamarchuk2708@gmail.com>
2024-11-23 00:16:04 +0200

    firsthw

---
+++ 01/main.js
@@ -0,0 +1,9 @@
+let userAgeValue = "22";
+let userNameNickname = "Max";
+
+userAgeValue = "28";
+userNameNickname = "Misha";
+
+
+console.log(userAgeValue); // 28
+console.log(userNameNickname); // Misha
