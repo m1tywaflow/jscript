9b536d7baf10c249fd040b18a8d50868e10540b9
m1tywaflow <dimamarchuk2708@gmail.com>
2024-11-23 00:52:06 +0200

    hello

--- 02/main.js
+++
@@ -1,9 +0,0 @@
-let userAgeValue = "22";
-let userNameNickname = "Vlad";
-
-userAgeValue = "24";
-userNameNickname = "Alex";
-
-
-console.log(userAgeValue); // 28
-console.log(userNameNickname); // Misha
--- 04/main.js
+++
@@ -1,2 +0,0 @@
-let age ="21";
-let name = "Alex";
