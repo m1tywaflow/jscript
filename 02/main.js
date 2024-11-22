a334af38a3fbafe034713363a2f5ef7ed0e0438b
m1tywaflow <dimamarchuk2708@gmail.com>
2024-11-23 00:21:15 +0200

    first commit

---
+++ 02/main.js
@@ -0,0 +1,9 @@
+let userAgeValue = "22";
+let userNameNickname = "Vlad";
+
+userAgeValue = "24";
+userNameNickname = "Alex";
+
+
+console.log(userAgeValue); // 28
+console.log(userNameNickname); // Misha
