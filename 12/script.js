d8856d67c8d97cef64e7e6f4972c99989f4beb8f
m1tywaflow <dimamarchuk2708@gmail.com>
2024-12-08 14:16:04 +0200

    hw

---
+++ 12/script.js
@@ -0,0 +1,23 @@
+let idElement = document.getElementById(`byid`);
+console.log(idElement);
+let classElement = document.getElementsByClassName(`section-one`);
+console.log(classElement);
+let tagElement = document.getElementsByTagName(`div`);
+console.log(tagElement);
+
+
+
+let firstLi = document.querySelector('ul li');
+console.log(firstLi);
+let lastLi = document.querySelector('ul li:last-child');
+console.log(lastLi);
+let thirdLi = document.querySelector('ul li:nth-child(3)');
+console.log(thirdLi);
+
+let allLi = document.querySelectorAll('ul li');
+console.log(allLi);
+let[...allLiArr] = document.querySelectorAll('ul li');
+console.log(allLiArr);
+
+let listItems = document.querySelectorAll('ul.nav > li');
+let listItem = document.querySelectorAll('li:nth-child(2)');
