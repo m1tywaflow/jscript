eb6a30ced1cae32b893fc0c251c402c8c150cbd8
m1tywaflow <dimamarchuk2708@gmail.com>
2024-12-09 14:38:46 +0200

    hwfix

--- 12/script.js
+++ 12/script.js
@@ -1,23 +1,32 @@
-let idElement = document.getElementById(`byid`);
-console.log(idElement);
-let classElement = document.getElementsByClassName(`section-one`);
-console.log(classElement);
-let tagElement = document.getElementsByTagName(`div`);
-console.log(tagElement);
 
+window.onload = function() {
+    let idElement = document.getElementById(`byid`);
+    console.log(idElement);
 
+    let classElement = document.getElementsByClassName(`section-one`);
+    console.log(classElement);
 
-let firstLi = document.querySelector('ul li');
-console.log(firstLi);
-let lastLi = document.querySelector('ul li:last-child');
-console.log(lastLi);
-let thirdLi = document.querySelector('ul li:nth-child(3)');
-console.log(thirdLi);
+    let tagElement = document.getElementsByTagName(`div`);
+    console.log(tagElement);
 
-let allLi = document.querySelectorAll('ul li');
-console.log(allLi);
-let[...allLiArr] = document.querySelectorAll('ul li');
-console.log(allLiArr);
+    let firstLi = document.querySelector('ul li');
+    console.log(firstLi);
 
-let listItems = document.querySelectorAll('ul.nav > li');
-let listItem = document.querySelectorAll('li:nth-child(2)');
+    let lastLi = document.querySelector('ul li:last-child');
+    console.log(lastLi);
+
+    let thirdLi = document.querySelector('ul li:nth-child(3)');
+    console.log(thirdLi);
+
+    let allLi = document.querySelectorAll('ul li');
+    console.log(allLi);
+
+    let [...allLiArr] = document.querySelectorAll('ul li');
+    console.log(allLiArr);
+
+    let listItems = document.querySelectorAll('ul.nav > li');
+    console.log(listItems);
+
+    let listItem = document.querySelectorAll('li:nth-child(2)');
+    console.log(listItem);
+};
